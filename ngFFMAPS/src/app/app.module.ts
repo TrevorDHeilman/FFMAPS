b7ba6ce6c885806db9628866e23b070eaf494f7f
James Kellenberger <jwkellenberger@gmail.com>
2019-06-11 20:15:23 -0400

    employee portal and basis for routing

--- ngFFMAPS/src/app/app.module.ts
+++ ngFFMAPS/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeePortalComponent } from './employee-portal/employee-portal.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule} from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeePortalComponent,
+    HomeComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule,
