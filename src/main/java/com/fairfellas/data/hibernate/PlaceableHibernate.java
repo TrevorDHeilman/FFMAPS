60a2d01ff4f8dd1b711f40810fee60ead1b85556
TrevorDHeilman <trevordheilman@gmail.com>
2019-06-12 20:23:23 -0400

    Placeable files

---
+++ src/main/java/com/fairfellas/data/hibernate/PlaceableHibernate.java
@@ -0,0 +1,76 @@
+package com.fairfellas.data.hibernate;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.fairfellas.beans.Placeable;
+import com.fairfellas.data.PlaceableDAO;
+import com.fairfellas.utils.HibernateUtil;
+import com.fairfellas.utils.LogUtil;
+
+
+public class PlaceableHibernate implements PlaceableDAO{
+	private HibernateUtil hu = HibernateUtil.getInstance();
+	
+	@Override
+	public int addPlaceable(Placeable newPlaceable) {
+		Session s = hu.getSession();
+		Transaction tx = null;
+		try {
+			tx = s.beginTransaction();
+			s.save(newPlaceable);
+			tx.commit();
+		} catch(Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			LogUtil.logException(e, PlaceableHibernate.class);
+		} finally {
+			s.close();
+		}
+		return newPlaceable.getPlaceableId();
+	}
+
+	@Override
+	public Placeable getPlaceableById(int id) {
+		Placeable placeable;
+		Session s = hu.getSession();
+		placeable = s.get(Placeable.class, id);
+		s.close();
+		return placeable;
+	}
+
+	@Override
+	public Set<Placeable> getPlaceableByType(String type) {
+		Session s = hu.getSession();
+		String query = "FROM Placeable";
+		Query<Placeable> q = s.createQuery(query, Placeable.class);
+		List<Placeable> placeableList = q.getResultList();
+		Set<Placeable> placeableSet = new HashSet<Placeable>();
+		placeableSet.addAll(placeableList);
+		return placeableSet;
+	}
+
+	@Override
+	public void removePlaceable(Placeable placeable) {
+		Session s = hu.getSession();
+		Transaction tx = null;
+		try {
+			tx = s.beginTransaction();
+			s.delete(placeable);
+			tx.commit();
+		} catch(Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			LogUtil.logException(e, PlaceableHibernate.class);
+		} finally {
+			s.close();	
+		}
+	}	
+}
