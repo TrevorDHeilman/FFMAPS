794df24c96dbb84895b476a7f0923b543a356cf3
Kenneth Tsai <kennethtsai7@gmail.com>
2019-06-13 20:34:03 -0400

    updated stock component

--- ngFFMAPS/src/app/stocks/stock/stock.component.ts
+++ ngFFMAPS/src/app/stocks/stock/stock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Stock } from '../stock';
 import { StockService } from '../stock.service';
 
@@ -8,6 +8,7 @@ import { StockService } from '../stock.service';
   styleUrls: ['./stock.component.css']
 })
 export class StockComponent implements OnInit {
+  @Input() stock: Stock;
   public stocks: Stock[];
   public newStock: Stock;
 
