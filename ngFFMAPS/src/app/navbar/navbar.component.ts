a6185190cd192311eee3ab4e30126acf6fe247cc
James Kellenberger <jwkellenberger@gmail.com>
2019-06-14 23:30:32 -0400

    linking user service to nav to track when not on home page

--- ngFFMAPS/src/app/navbar/navbar.component.ts
+++ ngFFMAPS/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  private onHome :string;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.onHome = "home";
   }
 
+  pushHome() {
+    this.onHome = this.userService.hitHome();
+  }
+
+  pushPortal(){
+    this.onHome = this.userService.hitPortal();
+  }
 }
