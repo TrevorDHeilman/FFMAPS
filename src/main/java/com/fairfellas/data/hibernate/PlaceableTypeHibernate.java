a94630cd7d66efc7fc6358ed6f2b6656342be6a9
James Kellenberger <jwkellenberger@gmail.com>
2019-06-21 13:07:36 -0400

    updated annotations

--- src/main/java/com/fairfellas/data/hibernate/PlaceableTypeHibernate.java
+++ src/main/java/com/fairfellas/data/hibernate/PlaceableTypeHibernate.java
@@ -7,13 +7,14 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import com.fairfellas.beans.Placeable;
 import com.fairfellas.beans.PlaceableType;
 import com.fairfellas.data.PlaceableTypeDAO;
 import com.fairfellas.utils.HibernateUtil;
 import com.fairfellas.utils.LogUtil;
 
+@Component
 public class PlaceableTypeHibernate implements PlaceableTypeDAO{
 	@Autowired
 	private HibernateUtil hu;
