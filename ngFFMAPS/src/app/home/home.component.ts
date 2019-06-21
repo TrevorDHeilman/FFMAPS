82f09684430a17d74aae96644f2f8c8489513f15
James Kellenberger <jwkellenberger@gmail.com>
2019-06-21 18:03:38 -0400

    modified home compoent showing pending events

--- ngFFMAPS/src/app/home/home.component.ts
+++ ngFFMAPS/src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public acceptString = "Accept";
-  public pendString = "Accept";
+  public pendString = "Pending";
 
   constructor() { }
 
